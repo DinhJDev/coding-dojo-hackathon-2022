8b150f2cb908ed494195dd2dbc80d22b6908ea2b
Aaron Hill <chillaaron21@gmail.com>
2022-06-24 21:15:57 -0400

    HotZone updated

---
+++ Controllers/HackathonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Hackathon.Dal;
+using Hackathon.Core;
+
+
+namespace hackathon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HackathonController : ControllerBase
+    {
+        private readonly ZoneService _hotZone;
+
+        public HackathonController(ZoneService hotZone)
+        {
+            _hotZone = hotZone;
+        }
+        [HttpGet(Name = "GetZone")]
+        public HotZone GetZone(int id)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            return _hotZone.GetOne(id);
+        }
+        [HttpPost(Name = "MarkZone")]
+        public void MarkZone(HotZone newT)
+        {
+            _hotZone.Create(newT);
+             
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Hackathon.Dal;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions { ContentRootPath = AppContext.BaseDirectory });
+var connectionString = builder.Configuration.GetConnectionString("HackathonContextConnection");
+
+var serverVersion = new MySqlServerVersion(new Version(11, 3, 0, 6371));
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.AddJsonFile("appsettings.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+}
+
 
 // Add services to the container.
 
@@ -6,6 +21,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ZoneService>();
+builder.Services.AddDbContext<HackathonContext>(opt => opt.UseMySql(builder.Configuration.GetConnectionString("connection"), serverVersion));
 
 var app = builder.Build();
 
