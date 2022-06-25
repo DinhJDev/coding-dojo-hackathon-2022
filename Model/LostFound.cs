2c4bf900037fa3a7213b3c14873f934ffd5ac57a
Aaron Hill <chillaaron21@gmail.com>
2022-06-24 23:07:22 -0400

    consolidated to one library

---
+++ Model/LostFound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Core
+{
+    public class LostFound
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? From { get; set; }
+        public string? Contact { get; set; }
+        [Required]
+        public string? LostFoundAge { get; set; }
+        public string? ContactType { get; set; }
+        [Required]
+        public string? Lat { get; set; }
+        [Required]
+        public string? Lon { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Model/Supply.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Core
+{
+    public class Supply
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Type { get; set; }
+        [Required]
+        public string SupplyAge { get; set; }
+        [Required]
+        public string? Lat { get; set; }
+        [Required]
+        public string? Lon { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
